dacece202a166a2bcbb22a9ddcbef5a53e32b4af
balqw <balqw777@gmail.com>
2021-02-18 15:27:32 +0200

    a bit fix

--- src/main/java/ua/com/foxminded/controllers/GroupController.java
+++ src/main/java/ua/com/foxminded/controllers/GroupController.java
@@ -5,14 +5,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import ua.com.foxminded.service.StudentGroupService;
+import ua.com.foxminded.service.GroupService;
 
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/groups")
 public class StudentGroupController {
 
-    private final StudentGroupService groupService;
+    private final GroupService groupService;
 
     @GetMapping
     public String readAll(Model model){
